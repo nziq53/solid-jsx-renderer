594301d8f832548aeaa73dfa03ae483a4d18b0b7
oligami-0424 <nziq53@gmail.com>
2023-08-13 11:38:52 +0900

    patch

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,29 @@
 import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import solidPlugin from 'vite-plugin-solid'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'), // エントリポイント
-      name: 'solid-jsx-renderer', // グローバル変数として公開するライブラリの変数名
-      fileName: 'solid-jsx-renderer', // 生成するファイルのファイル名を指定します。
-      formats: ['es', 'umd'], // デフォルトのまま
-    },
-    rollupOptions: {
-      external: ['solid-js'],
-      output: {
-        globals: {
-          'solid-js': 'solid-js'
-        }
-      }
-    }
-  },
+  // build: {
+  //   lib: {
+  //     entry: resolve(__dirname, 'src/index.ts'), // エントリポイント
+  //     name: 'solid-jsx-renderer', // グローバル変数として公開するライブラリの変数名
+  //     fileName: 'solid-jsx-renderer', // 生成するファイルのファイル名を指定します。
+  //     formats: ['es', 'umd'], // デフォルトのまま
+  //   },
+  //   rollupOptions: {
+  //     external: ['solid-js'],
+  //     output: {
+  //       globals: {
+  //         'solid-js': 'solid-js'
+  //       }
+  //     }
+  //   }
+  // },
   plugins: [
-    solid()
+    solidPlugin()
   ],
+  optimizeDeps: {
+    exclude: ['solid-jsx-renderer']
+  }
 })
